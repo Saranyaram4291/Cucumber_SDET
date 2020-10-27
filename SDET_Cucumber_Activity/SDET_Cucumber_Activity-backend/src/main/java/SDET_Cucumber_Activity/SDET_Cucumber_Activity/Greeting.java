9d680341540cf1ba32f3db2d306ad921ba8fa407
Saranyaram4291 <70197800+Saranyaram4291@users.noreply.github.com>
2020-10-27 14:06:31 +0530

    Add files via upload

---
+++ SDET_Cucumber_Activity/SDET_Cucumber_Activity-backend/src/main/java/SDET_Cucumber_Activity/SDET_Cucumber_Activity/Greeting.java
@@ -0,0 +1,21 @@
+package SDET_Cucumber_Activity.SDET_Cucumber_Activity;
+
+public class Greeting {
+
+    private final long id;
+
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
